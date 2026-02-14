6ad86bd67f14b25166a809e53d7d093e352e7e8a
MWmdauguet <m.dauguet@mentalworks.fr>
2026-02-14 19:14:45 +0100

    optimisation

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // ← Pas 127.0.0.1 !
+    host: '0.0.0.0',
     port: 5173,
     strictPort: true,
     watch: {
-      usePolling: true,  // Important pour Docker
+      usePolling: true, 
     },
   }
 })
